4abb92e7540bf42084a16425301a22737b2db470
David Morales <davmorales@ucdavis.edu>
2020-08-12 00:38:56 -0400

    Changed the datatype of course id and student id in Registration.java just for inputting values in their corresponding databases. Must complete TODOS for student.js.

--- src/main/resources/static/student.js
+++ src/main/resources/static/student.js
@@ -26,7 +26,7 @@ function getStudents(data){
     var emailFromLoginPage = document.getElementById("login").value;
     var purpose = StudentProfile
 
-    for(index = 0; index < students; index++) {
+    for(index = 0; index < students.length; index++) {
         if(students[index].email === emailFromLoginPage) {
             var tableHtml = ''
                 + '<td>' + students[index].studentId + '</td>'
@@ -47,8 +47,63 @@ function getStudents(data){
 
 }
 
+function getCourses() {
+    var url = "/courseApi/courses"
+
+    var xhttpList = new XMLHttpRequest();
+
+    // Read JSON - and put in storage
+    xhttpList.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+            sessionStorage.setItem("courses", this.responseText);
+        }
+    };
+    xhttpList.open("GET", url, false);
+    xhttpList.send();
+    console.log("courses retrieved");
+
+    return sessionStorage.getItem("courses");
+
+}
+
+function getRegistrationList() {
+    var url = "/regApi/registration"
+
+    var xhttpList = new XMLHttpRequest();
+
+    // Read JSON - and put in storage
+    xhttpList.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+            sessionStorage.setItem("reg", this.responseText);
+        }
+    };
+    xhttpList.open("GET", url, false);
+    xhttpList.send();
+    console.log("registration list retrieved");
+
+    return sessionStorage.getItem("reg");
+
+}
+
+
+//TODO must find all the coursesIDs from the registration table
+//TODO add the courseID's to an array in javascript
+//TODO then search in the courses table by doing a double for loop
+//TODO outer for loop will iterate based of the courseID in the array
+//TODO innerloop will fetch the course from the course table
 function renderCourses(purpose, id) {
-    var regList =
-    var courses =
+    var pregList = getRegistrationList();
+    var pcourses = getCourses();
+    var regList = JSON.parse(pregList);
+    var courses = JSON.parse(pcourses);
+    var studentCourses = [];
+    // find all the courses id from the registration list by matching the student id
+    for(index = 0; index < regList.length; index++) {
+        if(regList[index].studentId === id) {
+
+        }
+    }
 
 }
